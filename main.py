05d1dcda4b604aec447f61948393cf5a23e2edbf
Arsalan Arghavan <arsalanarghavan2014@gmail.com>
2025-09-26 01:53:48 +0330

    fixing bugs

--- main.py
+++ main.py
@@ -132,15 +132,15 @@ def format_experience(exp, md_version: int = 2, redacted=False) -> str:
     def remove_emojis(text):
         emoji_pattern = re.compile(
             "["
-            "\U0001F600-\U0001F64F"
-            "\U0001F300-\U0001F5FF"
-            "\U0001F680-\U0001F6FF"
-            "\U0001F700-\U0001F77F"
-            "\U0001F780-\U0001F7FF"
-            "\U0001F800-\U0001F8FF"
-            "\U0001F900-\U0001F9FF"
-            "\U0001FA00-\U0001FA6F"
-            "\U0001FA70-\U0001FAFF"
+            "\U0001F600-\U0001F64F"  # emoticons
+            "\U0001F300-\U0001F5FF"  # symbols & pictographs
+            "\U0001F680-\U0001F6FF"  # transport & map symbols
+            "\U0001F700-\U0001F77F"  # alchemical symbols
+            "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+            "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+            "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+            "\U0001FA00-\U0001FA6F"  # Chess Symbols
+            "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
             "\U00002702-\U000027B0"
             "\U000024C2-\U0001F251"
             "]+", flags=re.UNICODE)
@@ -148,10 +148,11 @@ def format_experience(exp, md_version: int = 2, redacted=False) -> str:
 
     def make_safe_tag(name: str) -> str:
         text_no_emoji = remove_emojis(name)
-        return re.sub(r'[\s\-_]+', '_', text_no_emoji)
+        # Replace spaces, half-spaces, and dashes with underscores
+        return re.sub(r'[\s\u200c\-_]+', '_', text_no_emoji)
 
     # Check if 'exp' is the dataclass or the SQLAlchemy model
-    is_dataclass = hasattr(exp, 'field_name')
+    is_dataclass = isinstance(exp, ExperienceData)
 
     field_name = def_md(exp.field_name if is_dataclass else exp.field.name)
     major_name = def_md(exp.major_name if is_dataclass else exp.major.name)
